c0033ca9609b8f298761ec251e32fe69449a10cd
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-04-06 03:42:02 -0500

    Validando

--- js/form.js
+++ js/form.js
@@ -1,5 +1,8 @@
 
-$( document ).ready(function() {
+$( document ).ready(init);
+
+function init()
+{
 	var datos ;
 	var dat = [];
 
@@ -16,4 +19,52 @@ $( document ).ready(function() {
 	 	localStorage.setItem('ocurrenciaslist',JSON.stringify(dat));
 	 	window.location='tabla-usuario.html';
 	});
-});
+}
+
+/***************************FUNCION QUE VALIDA QUE SELCCIONES UN CARRO Y UN DESTINO*******************/
+function validarFormulario()
+{
+    var isValid=false;
+    if ($('#ocurrenciadesc').val() != '') 
+    {
+    	isValid=true;
+	}
+	return isValid;	
+}
+
+function validaHoraSalida()
+{
+	var isvalid=false;
+	if($('#horaSalida').val()>9 && $('#horaSalida').val()<19)
+   {
+   		isvalid=true;
+   }
+   return  isvalid;
+}
+
+function validaHoraRetorno()
+{
+	var isvalid=false;
+	if($('#horaRetorno').val()>9 && $('#horaRetorno').val()<19)
+   {
+   		isvalid=true;
+   }
+   return  isvalid;
+}
+
+function validaHoras()
+{
+	var isvalid=false;
+	if(validaHoraRetorno() && validaHoraSalida() )
+	{
+		if($('#horaSalida').val() < $('#horaRetorno').val())
+	    {
+	   		isvalid=true;
+	   		swal({
+		    title: "¡Se selecciono exitosamente!",
+		    imageUrl: ""
+			});
+	    }
+	}
+   	return  isvalid;
+}
